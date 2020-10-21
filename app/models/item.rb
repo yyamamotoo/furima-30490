5d7105a52eb429484c69ccadd1ec16313f40d33d
yyamamotoo <71579495+yyamamotoo@users.noreply.github.com>
2020-10-21 11:59:15 +0900

    商品実装修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,16 +9,19 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :name, presence: true
-  validates :info, presence: true
-  validates :item_category_id, presence: true
-  validates :item_sales_status_id, presence: true
-  validates :item_shipping_fee_status_id, presence: true
-  validates :item_prefecture_id, presence: true
-  validates :item_scheduled_delivery_id, presence: true
-  validates :image, presence: true
+  with_options presence: true do
+    validates :name
+    validates :info
+    validates :item_category_id
+    validates :item_sales_status_id
+    validates :item_shipping_fee_status_id
+    validates :item_prefecture_id
+    validates :item_scheduled_delivery_id
+    validates :image
+    validates :price
+  end
+
   validates :price, numericality: { only_integer: true, message: "Half-width characters" }
-  validates :price, presence: true
   validates :price, numericality: { :greater_than_or_equal_to => 300, :less_than_or_equal_to => 9999999 }
   validates :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id, :item_prefecture_id, :item_scheduled_delivery_id, numericality: { other_than: 1, message: "select" }
 end
