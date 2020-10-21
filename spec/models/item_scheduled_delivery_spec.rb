d8a288d39e53962d105fbb44869ec2f468f83b0a
yyamamotoo <71579495+yyamamotoo@users.noreply.github.com>
2020-10-21 09:08:40 +0900

    商品出品機能実装

---
+++ spec/models/item_scheduled_delivery_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe ItemScheduledDelivery, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
---
+++ spec/models/item_shipping_fee_status_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe ItemShippingFeeStatus, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
