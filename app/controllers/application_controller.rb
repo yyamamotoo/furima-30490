460bd650b629bc76346791bc0a2e766116a921bb
yyamamotoo <71579495+yyamamotoo@users.noreply.github.com>
2020-10-28 16:13:49 +0900

    basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_date])
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "yamamoto" && password == "30490"
+    end
   end
 end
